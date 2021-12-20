521ef0a1b22b39e648efa370df930464f4353d79
sm9171 <sm9171@nate.com>
2021-12-20 20:42:16 +0900

    Initialized Project

---
+++ src/main/java/beside/xxx/xxxserver/XxxServerApplication.java
@@ -0,0 +1,13 @@
+package beside.xxx.xxxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beside/xxx/xxxserver/XxxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package beside.xxx.xxxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
