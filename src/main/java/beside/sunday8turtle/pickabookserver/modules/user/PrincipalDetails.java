540133e3ca4c573f757ceb9ddb03907a0d34f191
uz2ni <uzini_@naver.com>
2021-12-28 23:36:08 +0900

    feat(login): Security User 정보 인터페이스 추가

---
+++ src/main/java/beside/sunday8turtle/pickabookserver/modules/user/PrincipalDetails.java
@@ -0,0 +1,59 @@
+package beside.sunday8turtle.pickabookserver.modules.user;
+
+import beside.sunday8turtle.pickabookserver.modules.user.domain.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoleList().forEach(r -> {
+            authorities.add(() -> r);
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
--- src/main/java/beside/sunday8turtle/pickabookserver/modules/user/service/UserService.java
+++ src/main/java/beside/sunday8turtle/pickabookserver/modules/user/service/UserService.java
@@ -1,8 +1,12 @@
 package beside.sunday8turtle.pickabookserver.modules.user.service;
 
+import beside.sunday8turtle.pickabookserver.modules.user.PrincipalDetails;
 import beside.sunday8turtle.pickabookserver.modules.user.domain.User;
 import beside.sunday8turtle.pickabookserver.modules.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -23,5 +27,11 @@ public class UserService {
 
     private String getEncodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new PrincipalDetails(user);
     }
 }
