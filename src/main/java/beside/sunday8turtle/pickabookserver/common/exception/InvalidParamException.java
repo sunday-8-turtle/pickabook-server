32ca4c1807805bbada86ff017018ae651d59d727
uz2ni <uzini_@naver.com>
2022-01-04 01:30:13 +0900

    feature(response): 응답 및 익셉션 공통 구조 적용
    * CommonControllerAdvice: 익셉션 공통 처리부
    * BaseException: 에상 가능한 기본 익셉션 클래스 (커스텀 익셉션이 필요한 경우 해당 익셉션 클래스를 상속받아 구현)
    * CommonResponse, ErrorCode: 응답용 클래스, 에러 코드 정의 클래스

---
+++ src/main/java/beside/sunday8turtle/pickabookserver/common/exception/InvalidParamException.java
@@ -0,0 +1,22 @@
+package beside.sunday8turtle.pickabookserver.common.exception;
+
+import beside.sunday8turtle.pickabookserver.common.response.ErrorCode;
+
+public class InvalidParamException extends BaseException {
+
+    public InvalidParamException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public InvalidParamException(String errorMsg) {
+        super(errorMsg, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public InvalidParamException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+}
